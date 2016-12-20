3c917a56a6837fa87b5f3b9e4dd652ea63ebeca5
Madhur Malhotra <malhotra.madhur15@gmail.com>
2016-12-19 22:04:39 -0600

    got program to return time used to create 100-word histogram

--- performance.py
+++ performance.py
@@ -4,14 +4,16 @@ from histogram import get_words_list
 from histogram import create_histogram
 
 def benchmark(file_name):
+    t0 = time.time()
     hundred_list = get_words_list(file_name, 100)
     hundred_hgram = create_histogram(hundred_list)
-    return hundred_hgram
+    t1 = time.time()
+    t100 = t1-t0
+    print t100
 
     ten_thousand_list = get_words_list(file_name, 100000)
     ten_thousand_hgram = create_histogram(ten_thousand_list)
-    return ten_thousand_hgram
 
 if __name__ == '__main__':
     file_name = str(sys.argv[1])
-    benchmark()
+    benchmark(file_name)
