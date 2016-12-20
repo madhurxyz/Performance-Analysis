095c3e42297b1559800859db58467529dc72d2a3
Madhur Malhotra <malhotra.madhur15@gmail.com>
2016-12-19 22:00:41 -0600

    got number_of_words to pass as an argument, histogram works

--- performance.py
+++ performance.py
@@ -5,7 +5,6 @@ from histogram import create_histogram
 def benchmark(file_name):
     hundred_list = get_words_list(file_name, 100)
     hundred_hgram = create_histogram(hundred_list)
-    print hundred_hgram
     return hundred_hgram
 
 if __name__ == '__main__':
