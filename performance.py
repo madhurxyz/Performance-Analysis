085723d9ae6d86fd09eea4ef5a73c6cbb5d533ad
Madhur Malhotra <malhotra.madhur15@gmail.com>
2016-12-19 22:01:47 -0600

    got create_histogram to return a histogram of ten thousand words

--- performance.py
+++ performance.py
@@ -7,6 +7,10 @@ def benchmark(file_name):
     hundred_hgram = create_histogram(hundred_list)
     return hundred_hgram
 
+    ten_thousand_list = get_words_list(file_name, 100000)
+    ten_thousand_hgram = create_histogram(ten_thousand_list)
+    return ten_thousand_hgram
+
 if __name__ == '__main__':
     file_name = str(sys.argv[1])
     benchmark()
